36ba83597c97fc74e74aaba5383b278823467f10
Bussylmz <ylmz0bsra@gmail.com>
2025-02-09 21:19:50 +0300

    first commit

---
+++ swiftUberUdemig/Controllers/Authentication/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  swiftUberUdemig
+//
+//  Created by Mehmet Seyhan on 9.02.2025.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swiftUberUdemig/Controllers/ContainerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContainerViewController.swift
+//  swiftUberUdemig
+//
+//  Created by Mehmet Seyhan on 9.02.2025.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
